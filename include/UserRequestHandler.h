5e5a2e716ec8e6561d45b6cd6f7fdb0be494898d
robertostoja-lniski <robert.lniski@gmail.com>
2019-11-17 18:16:10 +0100

    Uzytkownik moze wpisywac komendy

---
+++ include/UserRequestHandler.h
@@ -0,0 +1,48 @@
+//
+// Created by robert on 17.11.2019.
+//
+
+#ifndef TINY_USERREQUESTHANDLER_H
+#define TINY_USERREQUESTHANDLER_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+enum RequestType{
+
+    NOT_A_REQUEST = -1,
+    SINGLE_REQUEST = 0,
+    DOUBLE_REQUEST = 1,
+};
+class UserRequestHandler{
+
+private:
+    // skorzystalem z wektora, poniewaz tablica musi miec podana dlugosc
+    // przy deklaracji, a chcialem ograniczyc dodawanie kolejnych oznaczen
+    const std::vector<std::string> singleCommands = {"lls", "ls", "ls-owners"};
+    const std::vector<std::string> doubleCommandsPrefix = {"get", "set-workspace", "add", "put", "sys-rm", "full-rm"};
+
+    // sprawdza czy pierwszy parametr jest poleceniem
+    // jesli tak to zwaraca rodzaj polecania ( jedno, lub dwuargumentowe )
+    // i w zaleznosci od rodzaju modyfikuje argumenty wejsciowe,
+    // w taki sposob ze przypisane im sa oddzielne slowa polecenia
+    // np po otrzymaniu:      get          /home/robert/abcd
+    // zwroci DOUBLE_REQUEST, drugi argument bedzie mial wartosc "get", a trzeci "/home/robert/abcd"
+    // po otrzymaniu        unixy   tiny
+    // zwroci NOT_A_REQUEST i nie zmieni argumentu drugiego i trzeciego
+    RequestType preprocessRequest(std::string, std::string&, std::string&);
+    // dla pojedynczych polecen
+    void processRequest(std::string);
+    // dla podwojnych polecen
+    void processRequest(std::string, std::string);
+    void printHelp();
+    void printErrorMessage();
+
+public:
+    UserRequestHandler() = default;
+    // jedyne publiczne polecenie, odbiera komendy i je przetwarza
+    void receiveRequest();
+};
+#endif //TINY_USERREQUESTHANDLER_H
